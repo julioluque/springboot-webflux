789b41cf27d22f9772c04e9174c1c4c6034b6bbd
julioluque <julio.a.luque@gmail.com>
2021-01-11 09:26:43 -0300

    Commit inicial

---
+++ src/test/java/com/jluque/springboot/webflux/app/SpringbootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jluque.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
